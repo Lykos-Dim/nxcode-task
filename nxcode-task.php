f3668b9d546626f131eac564c97d795c605af13c
Lykos-Dim <lykos@psdgator.com>
2024-03-26 22:16:36 +0200

    structure of the project

--- nxcode-task.php
+++ nxcode-task.php
@@ -7,20 +7,25 @@
  */
 
 // Include widget functionality.
-require_once(plugin_dir_path(__FILE__) . 'widget-doctors.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/widget-doctors.php');
 
 // Include archive page functionality.
-require_once(plugin_dir_path(__FILE__) . 'doctors-archive-page.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/doctors-archive-page.php');
 
 // Include admin functionality.
-require_once(plugin_dir_path(__FILE__) . 'admin-functions.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/admin-functions.php');
 
 // Include article linking functionality.
-require_once(plugin_dir_path(__FILE__) . 'article-linking.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/article-linking.php');
 
 // Enqueue styles and scripts.
 function enqueue_doctors_styles_scripts() {
-    // Enqueue styles and scripts here.
+	$version = time();
+
+	wp_enqueue_style( 'nxcode-task-css', plugin_dir_url( __FILE__ ) . '/assets/css/main.css', '', $version );
+	wp_enqueue_script( 'nxcode-task-js', plugin_dir_url( __FILE__ ) . '/assets/js/main.js', array( 'jquery' ), $version, true );
+
+
 }
 add_action('wp_enqueue_scripts', 'enqueue_doctors_styles_scripts');
 
